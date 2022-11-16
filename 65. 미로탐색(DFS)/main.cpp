67348efe1cac6954b25143db3e9a3b81d5870688
Jucy92 <97105334+Jucy92@users.noreply.github.com>
2022-11-16 22:01:36 +0900

    Add files via upload
    출력 오류 수정

---
+++ 65. 미로탐색(DFS)/main.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+using namespace std;
+
+int map[10][10], chk[10][10];
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, 1, 0, -1};
+int cnt=0;
+
+void DFS(int x, int y){
+	int i, xx, yy;
+	if(x==7 && y==7){
+		cnt++;
+	}
+	else{
+		for(i=0; i<4; i++){
+			xx = x+dx[i];
+			yy = y+dy[i];
+			if(xx<1 || xx>7 || yy<1 || yy>7) continue;
+			if(map[xx][yy]==0 && chk[xx][yy]==0){
+				chk[xx][yy]=1;
+				DFS(xx,yy);
+				chk[xx][yy]=0;
+			}
+		}
+	}
+}
+int main(){
+	freopen("input.txt", "rt", stdin);
+	int i, j;
+	
+	for(i=1; i<=7; i++){
+		for(j=1; j<=7; j++){
+			scanf("%d", &map[i][j]);
+		}
+	}
+//	for(i=1; i<=7; i++){
+//		for(j=1; j<=7; j++){
+//			printf("%d ", map[i][j]);
+//		}
+//		printf("\n");
+//	}
+	chk[1][1]=1;
+	DFS(1, 1);
+	printf("%d\n", cnt);
+	 
+	return 0;
+}
