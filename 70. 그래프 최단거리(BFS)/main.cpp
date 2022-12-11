a37bc9fbaba8a588cace09a0f7ad671daceda37a
Jucy92 <97105334+Jucy92@users.noreply.github.com>
2022-12-11 23:47:26 +0900

    내방법으로 처리 (Q에 대해서 Front, Back으로 변수 만들어서 처리)

--- 70. 그래프 최단거리(BFS)/main.cpp
+++ 70. 그래프 최단거리(BFS)/main.cpp
@@ -3,8 +3,9 @@
 #include<algorithm>
 
 using namespace std;	
-int chk[30], n, front=-1, back=-1, Q[30];
-vector <int> map[n+1]; 		//여유되면 map[n+1] / map(n+1) 에 푸시백 써서 다른적 체크해보기  
+int chk[30], n, front=-1, back=-1, Q[30], cnt[30];
+//vector <int> map[n+1]; 		//여유되면 map[n+1] / map(n+1) 에 푸시백 써서 다른적 체크해보기  
+vector <int> map[30];
 	
 int main(){
 	freopen("input.txt", "rt", stdin);
@@ -16,15 +17,24 @@ int main(){
 		map[a].push_back(b);
 	}
 	Q[++back]=1;
+	chk[1]=1;
 	
 	while(front<back){
 		x=Q[++front];
-		for(i=0; i<map[i].size(); i++){
-			Q[++back]=map[x][i];
+		
+//		printf("%d ",x);
+		for(i=0; i<map[x].size(); i++){
+			if(chk[map[x][i]]==0){
+				chk[map[x][i]]=1;
+				Q[++back]=map[x][i];
+				cnt[map[x][i]]=cnt[x]+1;
+			}
+			
 		}
 	}
-	chk[1]=1;
-	DFS(1, 0);
-	printf("%d\n", mini);
+	for(i=2; i<=n; i++){
+		printf("%d : %d\n", i, cnt[i]);
+	}
+
 	return 0;
 }
