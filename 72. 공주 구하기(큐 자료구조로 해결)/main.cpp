b7416e919c0353b3bcf1d9e70bfa1610adcc3ffa
Jucy92 <97105334+Jucy92@users.noreply.github.com>
2022-12-15 23:47:53 +0900

    진행 중

---
+++ 72. 공주 구하기(큐 자료구조로 해결)/main.cpp
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#define N 10001
+using namespace std;
+int chk[1001], val[1001];
+
+int main(){
+	freopen("input.txt", "rt", stdin);
+	int n, k, i, x;
+	queue<int> Q;
+	scanf("%d %d", &n, &k);
+	
+	for(i=1; i<=n; i++){
+		scanf("%d", &val[i]);
+	}
+
+	
+//	/*
+	Q.push(1);
+//	chk[S]=1;
+	
+	while(!Q.empty()){
+		x=Q.front();
+		Q.pop();
+		if(x=)
+	}
+	
+	
+	
+//	*/
+	return 0;
+}
