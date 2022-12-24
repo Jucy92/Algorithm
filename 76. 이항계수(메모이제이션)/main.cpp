95538dfeb3c9d5c81f11ac6891152a313ebf69e8
Jucy92 <97105334+Jucy92@users.noreply.github.com>
2022-12-24 20:37:12 +0900

    메모이제이션으로 처리

--- 76. 이항계수(메모이제이션)/main.cpp
+++ 76. 이항계수(메모이제이션)/main.cpp
@@ -3,9 +3,11 @@
 #include <vector>
 #include <queue>
 using namespace std;
+int dy[21][21];
 int DFS(int n, int k){
+	if(dy[n][k]>0) return dy[n][k];
 	if(n==k || k==0) return 1;
-	else return DFS(n-1, k-1)+ DFS(n-1,k);
+	else return dy[n][k]=DFS(n-1, k-1)+ DFS(n-1,k);
 }
 
 int main(){
