dcc433862ac18a06a82e4041ac65204347514306
Jucy92 <97105334+Jucy92@users.noreply.github.com>
2022-12-26 19:10:54 +0900

    스택 사용해서 처리

---
+++ 77. 친구인가(Union & Find)/main.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+#include <queue>
+using namespace std;
+int DFS(int a, int b){
+//	if()
+	return 0;
+}
+
+int main(){
+	freopen("input.txt", "rt", stdin);
+	int n, m, a, b, i;
+	
+	scanf("%d %d", &n, &m);
+	printf("%d %d\n", &n, &m);
+	
+	for(i=1; i<=m; i++){
+		scanf("%d %d\n", &a, &b);
+	}
+	scanf("%d %d\n",&a, &b);
+	DFS(a, b);
+	
+	
+	return 0;
+}
+
