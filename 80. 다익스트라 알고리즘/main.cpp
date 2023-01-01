18277a9d8347610fb151b06c3d9eae16c3823dd5
Jucy92 <97105334+Jucy92@users.noreply.github.com>
2023-01-01 22:58:23 +0900

    푸는 중

--- 80. 다익스트라 알고리즘/main.cpp
+++ 80. 다익스트라 알고리즘/main.cpp
@@ -8,8 +8,12 @@ int ch[30], dist[30];
 vector<pair<int, int> > map[30];
 int main(){
 	freopen("input.txt", "rt", stdin);
-	int n, m, i;
+	int n, m, i, a, b, c;
 	scanf("%d %d", &n, &m);
+	
+	for(i=1; i<=m; i++){
+		scanf("%d %d %d", &a, &b, &c);
+	}
 	
 	return 0;
 }
