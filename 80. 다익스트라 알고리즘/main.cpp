9e92a8c496c56cf6cc7fdac31170bac6b04b14e1
Jucy92 <97105334+Jucy92@users.noreply.github.com>
2023-01-08 23:29:52 +0900

    푸는 중
    어떤 방식으로 풀어야 하는 지 해보는 중... 스택, 큐?

--- 80. 다익스트라 알고리즘/main.cpp
+++ 80. 다익스트라 알고리즘/main.cpp
@@ -4,8 +4,20 @@
 #define x first
 #define y second
 using namespace std;	
-int ch[30], dist[30];
+int ch[30], dist[30], sum;
 vector<pair<int, int> > map[30];
+
+int DFS(int v, int b, int sum){
+	int i;
+	if(v==b) return 0;// 꾸역 하려면..  DFS(1,v,sum)으로 해서 if (v==i+1으로 하면서 하면되려나.. 
+	else{
+		for(i=0; i<map[b].size(); i++){
+			DFS(b+1, v, sum+map[b][i].x);	
+		}
+		return 0;
+	}
+}
+
 int main(){
 	freopen("input.txt", "rt", stdin);
 	int n, m, i, a, b, c;
@@ -13,7 +25,14 @@ int main(){
 	
 	for(i=1; i<=m; i++){
 		scanf("%d %d %d", &a, &b, &c);
+		map[a].push_back(make_pair(b,c));
+	} 
+	for(i=2;i<=n;i++){
+		sum=0;
+//		DFS(i, 1, sum);	// 재귀로 처리 못함
+		// 정점을 던져주고.. map에서 1정점을 기준으로 각 정점에 연결된 모든 간선을  
 	}
 	
 	return 0;
 }
+
