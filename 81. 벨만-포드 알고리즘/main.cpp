cd317e8a9ec84324ff2a985c436226721abfeeeb
Jucy92 <97105334+Jucy92@users.noreply.github.com>
2023-01-14 15:57:27 +0900

    풀긴 했는데.. 내용이 완벽하게 이해 안됨 -> 복습 필요

--- 81. 벨만-포드 알고리즘/main.cpp
+++ 81. 벨만-포드 알고리즘/main.cpp
@@ -1,39 +1,35 @@
-#include <stdio.h>
-#include <queue>
-#include <vector>
+#include<stdio.h>
+#include<algorithm>
+#include<queue>
+#include<vector>
 using namespace std;
 int dist[101];
 struct Edge{
-	int s, e, val;
+	int s;
+	int e;
+	int val;
 	Edge(int a, int b, int c){
 		s=a;
 		e=b;
 		val=c;
 	}
-	/*
-	bool operator <(Edge &b){
-		return val>b.val;
-	}*/
 };
 
-int main() {
-	freopen("input.txt","rw",stdin);
-	int n, m, a, b, c, i, j;
-//	priority_queue <Edge> Q;
+int main(){
+	freopen("input.txt", "rt", stdin);
 	vector<Edge> Ed;
+	int i, n, m, a, b, c, j;
 	scanf("%d %d", &n, &m);
-	
-	for(i=1; i<=n; i++){
-		dist[i] = 2147000000;
-	}
 	for(i=1; i<=m; i++){
 		scanf("%d %d %d", &a, &b, &c);
-		Ed.push_back(Edge(a, b, c));
+		Ed.push_back(Edge(a, b, c));	
+	}
+	for(i=1; i<=n; i++){
+		dist[i]=2147000000;
 	}
 	scanf("%d %d", &a, &b);
 	dist[a]=0;
-//	printf("%d", Ed.size();
-	for(i=1; i<n; i++){
+	for(i=1; i<n; i++){  
 		for(j=0; j<Ed.size(); j++){
 			int u=Ed[j].s;
 			int v=Ed[j].e;
@@ -41,6 +37,17 @@ int main() {
 			if(dist[u]!=2147000000 && dist[u]+w<dist[v]){
 				dist[v]=dist[u]+w;
 			}
-		}	
+		}
 	}
+	for(j=0; j<Ed.size(); j++){
+		int u=Ed[j].s;
+		int v=Ed[j].e;
+		int w=Ed[j].val;
+		if(dist[u]!=2147000000 && dist[u]+w<dist[v]){
+			printf("-1\n");
+			exit(0);
+		}
+	}
+	printf("%d\n", dist[b]);
+	return 0;
 }
