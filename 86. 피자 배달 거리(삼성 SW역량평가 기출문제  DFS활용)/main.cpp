25e8ae1c313e9f161e89b7779a26e20156cc3308
Jucy92 <97105334+Jucy92@users.noreply.github.com>
2023-02-21 21:38:13 +0900

    내가 풀던 방법으로 해결

--- 86. 피자 배달 거리(삼성 SW역량평가 기출문제  DFS활용)/main.cpp
+++ 86. 피자 배달 거리(삼성 SW역량평가 기출문제  DFS활용)/main.cpp
@@ -1,45 +1,50 @@
-#include<bits/stdc++.h>
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
 using namespace std;
-vector<pair<int, int> > pz;
-vector<pair<int, int> > hs;
-int ch[20], m, res=2147000000, dis, sum=0;
-void DFS(int L, int s){
+int m, res=2147000000, ch[20], sum, dis;
+vector <pair <int, int> > P;
+vector <pair <int, int> > H;
+void DFS(int L, int v){
 	if(L==m){
+		int x1,x2,y1,y2;
 		sum=0;
-		for(int i=0; i<hs.size(); i++){
-			int x1=hs[i].first;
-			int y1=hs[i].second;
+		for(int i=0; i<H.size(); i++){
+			x1=H[i].first;
+			y1=H[i].second;
 			dis=2147000000;
 			for(int j=0; j<m; j++){
-				int x2=pz[ch[j]].first;
-				int y2=pz[ch[j]].second;
-				dis=min(dis, abs(x1-x2)+abs(y1-y2));
+				x2=P[ch[j]].first;
+				y2=P[ch[j]].second;
+				if(dis>(abs(x1-x2)+abs(y1-y2))){
+					dis=(abs(x1-x2)+abs(y1-y2));
+				}	
+				
 			}
-			sum=sum+dis;
+			sum+=dis;
 		}
-		if(sum<res) res=sum;
-	}
-	else{
-		for(int i=s; i<pz.size(); i++){
+		if(sum<res) res=sum; 
+	}else{
+		for(int i=v; i<P.size(); i++){
 			ch[L]=i;
 			DFS(L+1, i+1);
 		}
 	}
 }
-
 int main(){
-	ios_base::sync_with_stdio(false); //c++ 입출력 속도 향상 
-	freopen("input.txt", "rt", stdin);
-	int n, a;
-	cin>>n>>m;
-	for(int i=1; i<=n; i++){
-		for(int j=1; j<=n; j++){
-			cin>>a;
-			if(a==1) hs.push_back(make_pair(i, j));
-			else if(a==2) pz.push_back(make_pair(i, j));
+//	freopen("input.txt", "rt", stdin);
+	int n, i, j, a;
+	scanf("%d %d", &n, &m);
+	
+	for(i=0;i<n;i++){
+		for(j=0;j<n;j++){
+			scanf("%d", &a);
+			if(a==1) H.push_back(make_pair(i,j));
+			else if (a==2) P.push_back(make_pair(i,j));
 		}
 	}
-	DFS(0, 0);
-	cout<<res;
-	return 0;
+	DFS(0,0);
+	printf("%d", res);
+	return 0;	
 }
+
