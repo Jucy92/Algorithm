52895a5af42e985ebcb168457c192ab327469369
Jucy92 <97105334+Jucy92@users.noreply.github.com>
2023-02-23 23:44:15 +0900

    해결 중..

---
+++ 88. 미로의 최단거리 통로(BFS 활용)/main.cpp
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#define m 7
+using namespace std;
+
+int dx[4]={0, 1, 0, -1};
+int dy[4]={-1, 0, 1, 0};
+int map[30][30];
+struct Loc{
+	int x,y;
+	Loc(int a, int b){
+		x=a;
+		y=b;
+	}
+};
+int main(){
+	freopen("input.txt", "rt", stdin);
+	int i,j, a, n, cnt=0, min=2147000000;
+	queue <Loc> Q;
+	for(i=1; i<=m; i++){
+		for(j=1; j<=m; j++){
+			scanf("%d", &map[i][j]);
+		}
+	}
+	for(i=1; i<=n; i++){		// 여기를 굳이 이렇게 안돌리고 
+		for(j=1; j<=n; j++){	// dx.size() 만큼 돌면서 Q.push(Loc(1,1));
+			if(map[i][j]==0){
+				Q.push(Loc(i,j));
+				//printf("%d\n", Q.front()); -> 1
+//				map[i][j]=0;
+				while(!Q.empty()){
+					Loc tmp =Q.front();
+					Q.pop();
+					for(int k=0; k<4; k++){
+						if(map[tmp.x+dx[k]][tmp.y+dy[k]]==0){
+							Q.push(Loc(tmp.x+dx[k], tmp.y+dy[k]));
+							map[tmp.x+dx[k]][tmp.y+dy[k]]=1;
+							cnt++;
+						}
+					}
+				}
+				if(cnt<min) min=cnt;
+			}
+		}
+	}
+	
+	/*
+	while(!Q.empty()){
+		a=Q.front();
+		for(i=1; i<=n; i++){
+			for(j=1; j<=n; j++){
+				if(map[i][j]==1){
+					map[i][j]=0;
+					for(int k=0; k<8; k++){
+						
+					}
+				}
+			}
+		}
+		for(i=0; i<8; i++){
+			if(map[dx[i]][dy[i]]==1)
+		}
+		
+		Q.pop();
+	}
+	*/
+	printf("%d\n",cnt);
+	return 0;
+}
