cb92c2a8e8b78ece88609a6ff5568a5052aaf872
Jucy92 <97105334+Jucy92@users.noreply.github.com>
2023-03-01 20:29:05 +0900

    풀긴 했는데 클린코드화 필요

--- 89. 토마토(BFS 활용)/main.cpp
+++ 89. 토마토(BFS 활용)/main.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 
 int map[1001][1001], ch[1001][1001];
-int dx[4] ={0,1,0,-1};
-int dy[4] ={-1,0,1,0};
+int dx[4] ={-1,0,1,0};
+int dy[4] ={0,1,0,-1};
 struct Loc{
 	int x,y;
 	Loc(int a, int b){
@@ -15,11 +15,11 @@ struct Loc{
 
 int main(){
 	freopen("input.txt", "rt", stdin);
-	int n, m, a, i, j, cnt=0;
+	int n, m, a, i, j, cnt=0, chk=0, res=-2147000000;
 	queue <Loc> Q;
 	scanf("%d %d", &m, &n); // m가로 n세로 
 	for(i=0; i<n; i++){		// 세로 i로 처리 
-		for(j=0; j<n; j++){
+		for(j=0; j<m; j++){
 			scanf("%d", &a);
 			map[i][j]=a;
 			if(a==1){
@@ -37,23 +37,31 @@ int main(){
 			for(i=0; i<4; i++){
 				xx= dx[i]+tmp.x; 
 				yy= dy[i]+tmp.y;
-				if(map[xx][yy]==0 && xx>=0 && xx<m && yy>=0 && yy<m){
+				if(map[xx][yy]==0 && xx>=0 && xx<n && yy>=0 && yy<m){
 					map[xx][yy]=1;
 					Q.push(Loc(xx,yy));
 					ch[xx][yy]=ch[tmp.x][tmp.y]+1;
-					if(min<ch[xx][yy]){
-						min=ch[xx][yy];	
+					if(res < ch[xx][yy]){
+						res = ch[xx][yy];	
 					} 
 				} 
 			}
 		}
-	} 
-	if(cnt==m*n){
+		for(i=0; i<n; i++){		// 처리 끝나고.. 0(안익은 토마토) 따로 체크.. 
+			for(j=0; j<m; j++){
+				if(map[i][j]==0){
+					chk++;
+				} 
+			}
+		}
+		if(cnt==m*n){
 		printf("0\n");
+		}	
+		else if(chk!=0) printf("-1\n");
+		else{
+			printf("%d\n", res);
+		}
 	}
-//	else if( 안익은거 있으면 0....){}
-	else{
-		printf("%d\n", min);
-	}
+	
 	return 0;
 }
