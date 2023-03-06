f22234e6abc3eaefe336b79bef83adb62277751d
Jucy92 <97105334+Jucy92@users.noreply.github.com>
2023-03-06 23:10:05 +0900

    푸는 중..

--- 90. 라이언 킹 심바/main.cpp
+++ 90. 라이언 킹 심바/main.cpp
@@ -13,17 +13,12 @@ struct State{
 		y=b;
 		dis=c;
 	}
-//	bool operator<(const State &bb)const{
-//		if(dis==bb.dis){
-//			if(x==bb.x) return y>bb.y;
-//			else return x>bb.x;
-//		}
-//		else return dis>bb.dis;
-//	}
-	bool operator<(const State &b)const{
-		if(dis!=b.dis) return dis>b.dis;
-		if(x!=b.x) return x>b.x;
-		else return y>b.y;
+	bool operator < (const State &b)const{
+		if(dis==b.dis){
+			if(x==b.x){
+				return y>b.y;
+			}else return x>b.x;
+		}else return dis>b.dis;
 	}
 };
 
@@ -61,27 +56,12 @@ int main(){
 		int y=tmp.y;
 		int z=tmp.dis;
 		if(map[x][y]!=0 && map[x][y]<simba.s){	
-			simba.x=x;
-			simba.y=y;
-			simba.ate++;
-			if(simba.ate>=simba.s) simba.sizeUp();
-			map[x][y]=0;
-			for(i=1; i<=n; i++){
-				for(j=1; j<=n; j++){
-					ch[i][j]=0;
-				}
-			}
-			while(!Q.empty()) Q.pop();
-			res=tmp.dis;
+			
 		}
 		for(i=0; i<4; i++){
-			int xx=x+dx[i];
-			int yy=y+dy[i];
-			if(xx<1 || xx>n || yy<1 || yy>n || map[xx][yy]>simba.s || ch[xx][yy]>0) continue;
-			Q.push(State(xx, yy, z+1));
-			ch[xx][yy]=1;		
+					
 		}
 	}
-	printf("%d\n", res);
+	
 	return 0;
 }
